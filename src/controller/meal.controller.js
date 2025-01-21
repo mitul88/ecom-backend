d0e607ae6776ca1081efb7e65b56df222117b5f2
mitul88 <shahed.rahman88@gmail.com>
2025-01-22 00:40:17 +0600

    meals pagination done

--- src/controller/meal.controller.js
+++ src/controller/meal.controller.js
@@ -13,4 +13,27 @@ module.exports.createMeal = async (req, res) => {
     return res.status(500).send({ message: "internal error occured" });
   }
 };
-module.exports.getMeal = async (req, res) => {};
+module.exports.getMeal = async (req, res) => {
+  const limit = req.query.limit || 10;
+  const page = req.query.page || 1;
+  const orderBy = req.query.orderBy || "desc";
+  try {
+    const menus = await prisma.menuItem.findMany({
+      select: {
+        id: true,
+        name: true,
+        description: true,
+        price: true,
+        created_at: true,
+      },
+      take: parseInt(limit),
+      skip: (page - 1) * limit,
+      orderBy: {
+        created_at: orderBy,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({ message: "internal error occured" });
+  }
+};
