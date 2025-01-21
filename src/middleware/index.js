9974d90b1092d9c42e50f282548c4860a7c74c18
mitul88 <shahed.rahman88@gmail.com>
2025-01-22 01:09:56 +0600

    cors added

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 module.exports = (app) => {
+  app.use(cors());
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
   app.use(cookieParser());
