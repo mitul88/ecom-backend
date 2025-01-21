ae40d51552e7a1a3a2514a3f84dcbd9c5fe1e362
mitul88 <shahed.rahman88@gmail.com>
2025-01-21 23:01:55 +0600

    sign up done

--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -1,7 +1,36 @@
+const { PrismaClient } = require("@prisma/client/extension");
+const { hashPassword } = require("../helper/password.helper");
+
+const prisma = new PrismaClient();
+
 module.exports.login = async (req, res) => {};
 module.exports.signup = async (req, res) => {
-    const data = req.body
-    try{
-        // const user = 
+  const data = req.body;
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        email: data["email"],
+      },
+    });
+
+    if (user) {
+      return res
+        .status(404)
+        .send({ status: "fail", message: "Email already exists" });
     }
+    const hashedPassword = await hashPassword(data["password"]);
+    await prisma.user.create({
+      data: {
+        name: data["name"],
+        phone: data["phone"],
+        email: data["email"],
+        password: hashedPassword,
+      },
+    });
+    return res.status(201).send({
+      message: "User created",
+    });
+  } catch (err) {
+    console.log(err);
+  }
 };
