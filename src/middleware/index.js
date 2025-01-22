96335683ef8cc3a51eef748cb76d000209e5070e
mitul88 <shahed.rahman88@gmail.com>
2025-01-22 14:07:14 +0600

    cors updated

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -2,7 +2,11 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 module.exports = (app) => {
-  app.use(cors());
+  app.use(
+    cors({
+      origin: "https://ecom-frontend-azure.vercel.app/",
+    })
+  );
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
   app.use(cookieParser());
